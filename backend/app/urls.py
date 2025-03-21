f4a0ef292fc48306f490f0bae5fad4020a62e6c9
Chris <104437487+Yeesbae@users.noreply.github.com>
2025-03-21 17:14:17 +0800

    Resolve conflict

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -6,15 +6,6 @@ from . import views
 
 urlpatterns = [
     path('properties/', views.PropertyView.as_view(), name='properties_list'),
-    
-    path('', views.main, name='main'),
-    path('api/create-dummy-user/', views.create_dummy_user, name='create_dummy_user'),
-    path('api/create-user-ID/', views.create_user_ID, name='create_user_ID'),
-    path('api/delete-user-ID/', views.delete_user_ID, name='delete_user_ID'),
-    path('api/delete-latest-user/', views.delete_latest_user, name='delete_latest_user'),
-    path('api/delete-all-user/', views.delete_all_user, name='delete_all_user'),
-    path('api/update-user-ID/', views.update_user_ID, name='update_user_ID'),
-
 ]
 
 
